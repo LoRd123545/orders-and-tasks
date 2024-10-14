094b8fab493880aadb34a18b636343eb649fa38d
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-14 14:55:39 +0200

    added exports

--- tasks/src/types/tasks/index.ts
+++ tasks/src/types/tasks/index.ts
@@ -1,11 +1,7 @@
-import type { Task } from "./Task.js";
-import type { CreateTaskDto } from "./CreateTaskDto.js";
-import type { UpdateTaskDto } from "./UpdateTaskDto.js";
-import { Status } from "./Status.js";
+import type { Task } from './Task.js';
+import type { CreateTaskDto } from './CreateTaskDto.js';
+import type { UpdateTaskDto } from './UpdateTaskDto.js';
+import { Status } from './Status.js';
+import type { TaskFindOptions } from './TaskFindOptions.js';
 
-export {
-  Task,
-  CreateTaskDto,
-  UpdateTaskDto,
-  Status,
-}
+export { Task, CreateTaskDto, UpdateTaskDto, Status, TaskFindOptions };
