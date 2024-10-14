21f8bfb95d4164855be401cac7fc5ef15a4621bb
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-14 14:57:03 +0200

    passing filters to service

--- tasks/src/controllers/tasks.controller.ts
+++ tasks/src/controllers/tasks.controller.ts
@@ -1,12 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
-import tasksService from "@app/services/tasks.service.js";
+import tasksService from '@app/services/tasks.service.js';
 
-import { httpCodes } from "@app/shared/index.js";
+import { httpCodes } from '@app/shared/index.js';
 
 const find = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const tasks = await tasksService.find();
+    const tasks = await tasksService.find({
+      limit: req.query.limit ? parseInt(req.query.limit.toString()) : undefined,
+      offset: req.query.offset
+        ? parseInt(req.query.offset.toString())
+        : undefined,
+      orderBy: req.query.orderBy?.toString(),
+      sortBy: req.query.sortBy?.toString() === 'asc' ? 'asc' : 'desc',
+      where: {
+        dueTo: req.query.dueTo
+          ? new Date(req.query.dueTo.toString())
+          : undefined,
+        description: req.query.description?.toString(),
+        name: req.query.name?.toString(),
+        status: req.query.status?.toString(),
+      },
+    });
 
     if (tasks.length === 0) {
       res.sendStatus(httpCodes.EMPTY_RESPONE);
@@ -16,7 +31,7 @@ const find = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const findOne = async (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
@@ -27,7 +42,7 @@ const findOne = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const create = async (req: Request, res: Response, next: NextFunction) => {
   const newTask = req.body;
@@ -38,7 +53,7 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const update = async (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
@@ -50,7 +65,7 @@ const update = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const remove = async (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
@@ -61,12 +76,12 @@ const remove = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 export default {
   find,
   findOne,
   create,
   update,
-  remove
-}
+  remove,
+};
