bd6b9e9db3871e568ce2e8e816fb8dc1e6640c94
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-23 16:33:20 +0200

    schema validation

--- products/src/middleware/index.ts
+++ products/src/middleware/index.ts
@@ -1,5 +1,7 @@
 import { handleErrors } from '@app/middleware/handleErrors.js';
+import { validateBySchema } from './validateBySchema.js';
 
 export default {
   handleErrors,
+  validateBySchema,
 };
--- products/src/routes/products.ts
+++ products/src/routes/products.ts
@@ -2,21 +2,52 @@ import { Router } from 'express';
 
 import productsController from '@app/controllers/products.controller.js';
 
+import middleware from '@app/middleware/index.js';
+
+import {
+  ProductFiltersSchema,
+  CreateProductSchema,
+  ProductComplexFiltersSchema,
+  UpdateProductSchema,
+  UuidSchema,
+} from '@app/schemas/index.js';
+
 const router = Router();
 
 // GET / <- get all products
-router.get('/', productsController.find);
+router.get(
+  '/',
+  middleware.validateBySchema(ProductFiltersSchema, 'query'),
+  productsController.find
+);
 
 // GET /:id <- get one product
-router.get('/:id', productsController.findOne);
+router.get(
+  '/:id',
+  middleware.validateBySchema(UuidSchema, 'params'),
+  productsController.findOne
+);
 
 // POST / <- create product
-router.post('/', productsController.create);
+router.post(
+  '/',
+  middleware.validateBySchema(CreateProductSchema, 'body'),
+  productsController.create
+);
 
 // PATCH /:id <- update product
-router.patch('/:id', productsController.update);
+router.patch(
+  '/:id',
+  middleware.validateBySchema(UuidSchema, 'params'),
+  middleware.validateBySchema(UpdateProductSchema, 'body'),
+  productsController.update
+);
 
 // DELETE /:id <- remove product
-router.delete('/:id', productsController.remove);
+router.delete(
+  '/:id',
+  middleware.validateBySchema(UuidSchema, 'params'),
+  productsController.remove
+);
 
 export default router;
