96d108d8bacd080d9417d7352e698a78b9294e22
LoRd123545 <kamilabbasi694@gmail.com>
2024-10-24 00:50:53 +0200

    removed bug

--- tasks/src/schemas/tasks/CreateTaskSchema.ts
+++ tasks/src/schemas/tasks/CreateTaskSchema.ts
@@ -6,7 +6,7 @@ export const CreateTaskSchema = z
   .object({
     name: z.string().max(50).optional(),
     status: z.enum(statuses).optional(),
-    dueTo: z.date().optional(),
+    dueTo: z.string().datetime().optional(),
     description: z.string().max(500).optional(),
   })
   .strict();
